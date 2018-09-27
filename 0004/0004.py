07025702d1d6d0de3538a21bc67045126153c1d4
hallucigenia <air.bylive@gmail.com>
2018-09-27 22:52:28 +0800

    update

--- 0004/0004.py
+++ 0004/0004.py
@@ -1,27 +1,12 @@
 __author__ = 'fansly'
 
-import re, collections
+import os
+import collections
+from string import punctuation
 
-def get_word_frequencies(file_name):
-    dic = {}
-    txt = open(file_name, 'r').read().splitlines()
-
-    n = 0
-    for line in txt:
-        line = re.sub(r'[.?!,""/]', ' ', line)
-        line = re.sub(r' - ', ' ', line)
-        for word in line.split():
-
-            if word[-1] == '-':
-                m = word[:-1]
-                n = 1
-                break
-            if n == 1:
-                word = m + word
-                n = 0
-            return word
-            dic.setdefault(word.lower(), 0)
-            dic[word.lower()] += 1
-
-
-get_word_frequencies("test.txt")
+with open('test.txt', 'r') as ef:
+    str1 = ef.read().split(' ')
+b = collections.Counter(str1)
+with open('result.txt', 'w') as result_file:
+    for key,value in b.items():
+        result_file.write(key+':'+str(value)+'\n')
