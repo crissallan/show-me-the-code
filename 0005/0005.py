cfb18f0bd508d157fcaf1e0943a87d5d137d296c
hallucigenia <air.bylive@gmail.com>
2018-09-28 22:06:59 +0800

    debug and create more bug

--- 0005/0005.py
+++ 0005/0005.py
@@ -1,4 +1,22 @@
+#-*- coding: UTF-8 -*-
+__author__ = 'fansly'
+
+import os
 from PIL import Image
 
-im = Image.open(:wq
+s = '/home/hallucigenia/圖片'
+s2 = os.path.join('s', 'output')
 
+def processImage(filesource, destsource, name):
+    im = Image.open(name)
+    w, h = im.size
+    im.thumbnail(w / h * 640, 640)
+    im.save(name, 'jpg')
+
+def run():
+    os.chdir(s)
+    for i in os.listdir(s):
+            processImage(s, s2, i)
+
+if __name__ == '__main__':
+    run()
